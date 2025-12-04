bd7d47dc685425b2ae4f9a7e13d2bd62715c5a36
Twinzz-git <elisamuelayala345@gmail.com>
2025-12-03 23:23:09 -0400

    Add SharedLibrary as submodule and reference it

--- tests/Smdb.Core.Test/obj/Debug/net9.0/Smdb.Core.Test.AssemblyInfo.cs
+++ tests/Smdb.Core.Test/obj/Debug/net9.0/Smdb.Core.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smdb.Core.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f665fa9671b5842bfb69b8c5a36d05acb52f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3f07027bdec7f86d37c6e1410f91dae245dab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smdb.Core.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smdb.Core.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
